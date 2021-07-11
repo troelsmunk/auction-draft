5466964a750b973f602462a4b30920dc48af582a
Troels Munk <tm@troelsmunk.com>
2021-07-11 12:01:21 +0200

    Added svelte-adapter-firebase

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
+import firebase from "svelte-adapter-firebase"
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    adapter: firebase(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
   },
