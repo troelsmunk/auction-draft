a4858d5eb1389637607e350814771f1e2fe99b19
Troels Munk <tm@troelsmunk.com>
2021-07-13 18:40:46 +0200

    Switch from firebase adapter to static

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<h1>Hej til Emilie</h1>
+<p>Jeg kan skrive alt muligt på nettet</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import firebase from "svelte-adapter-firebase"
+import adapter from "@sveltejs/adapter-static"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: firebase(),
+    adapter: adapter({
+      fallback: "index.html",
+    }),
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+    ssr: false,
   },
 }
 
