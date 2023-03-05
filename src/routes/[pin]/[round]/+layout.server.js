34ee4b8b64a50c1ec0aa4e56fe9aa78138878d61
Troels Munk <tm@troelsmunk.com>
2023-03-05 17:55:52 +0100

    Working example for loading data in layouts

---
+++ src/routes/[pin]/[round]/+layout.server.js
@@ -0,0 +1,12 @@
+import { admin } from "$lib/admin.server"
+
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load({ params }) {
+  const auctionSize = await admin
+    .database()
+    .ref(`auctions/${params.pin}/size`)
+    .get()
+  return {
+    size: auctionSize.val(),
+  }
+}
---
+++ src/routes/[pin]/[round]/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Auth from "$lib/Auth.svelte"
+  import Firebase from "$lib/Firebase.svelte"
+  import Debugger from "$lib/Debugger.svelte"
+  import { auth, pin, uid } from "$lib/stores"
+  import { signOut } from "@firebase/auth"
+  import Database from "$lib/Database.svelte"
+  export let data
+</script>
+
+<h3>In an auction</h3>
+<h3>Scoreboard</h3>
+<p>There are {data.size} bidders in the auction</p>
+<slot />
