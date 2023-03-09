35b4261a104a924a5cb1ef0794dcbd422fd25240
Troels Munk <tm@troelsmunk.com>
2023-03-09 20:38:38 +0100

    Correct round to be number in readys

--- src/routes/[pin]/[round]/+page.server.js
+++ src/routes/[pin]/[round]/+page.server.js
@@ -5,9 +5,10 @@ export const actions = {
   submit: async (event) => {
     const formData = await event.request.formData()
     const uid = event.cookies.get("firebaseuid")
+    const round = parseInt(event.params.round)
     const auctionRef = admin.database().ref("auctions/" + event.params.pin)
     await auctionRef.child("/bids/" + uid).set(formData.get("bids"))
-    await auctionRef.child("/readys/" + uid).set(event.params.round)
+    await auctionRef.child("/readys/" + uid).set(round)
     return { success: true }
   },
 }
