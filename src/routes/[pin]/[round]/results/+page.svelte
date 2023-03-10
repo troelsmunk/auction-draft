bd012847bdb0cdd841f5c70cf46b3ac7c1d901d6
Troels Munk <tm@troelsmunk.com>
2023-03-10 20:59:21 +0100

    First pass at results page

--- src/routes/[pin]/[round]/results/+page.svelte
+++ src/routes/[pin]/[round]/results/+page.svelte
@@ -1,3 +1,33 @@
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data
+
+  let results = {
+    "1": {
+      winner: "blue",
+      bid: 5,
+    },
+    "2": {
+      winner: "green",
+      bid: 5,
+    },
+    "3": {
+      winner: "blue",
+      bid: 5,
+    },
+    //...
+    "15": {
+      winner: "red",
+      bid: 2,
+    },
+  }
+</script>
+
 <h3>This are results</h3>
-<p>wauw</p>
-<p>TODO Show results</p>
+
+<!-- TODO data.results instead of result -->
+{#each Object.entries(results) as [i, card]}
+  <p style:color={card.winner}>
+    {card.bid}
+  </p>
+{/each}
