f20cb99f10fe084b7dbd5c397df1ebc61f09ea0c
Troels Munk <tm@troelsmunk.com>
2023-03-12 21:46:16 +0100

    Sketch grid for results

--- src/routes/[pin]/[round]/results/+page.svelte
+++ src/routes/[pin]/[round]/results/+page.svelte
@@ -11,8 +11,34 @@
 <a href={nextRoundBiddingAddress()}>Next round </a>
 
 <h3>Results</h3>
-{#each Object.entries(JSON.parse(data.results)) as [i, card]}
-  <p style:color={data.colors[card.winner]}>
-    {card.bid}
-  </p>
-{/each}
+<div class="grid-container">
+  {#each Object.entries(JSON.parse(data.results)) as [i, card]}
+    <div
+      class="result-wrapper"
+      style:background-color={data.colors[card.winner]}
+    >
+      <div class="result-inner">
+        {card.bid}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+
+  .result-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+  }
+
+  .result-inner {
+    align-self: center;
+    background-color: antiquewhite;
+  }
+</style>
