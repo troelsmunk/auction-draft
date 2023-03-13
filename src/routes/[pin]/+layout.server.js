3c5eae6f234085272b82e4dd4d7dbdb70401ce4a
Troels Munk <tm@troelsmunk.com>
2023-03-13 18:26:14 +0100

    Added better colors, hex-coded

--- src/routes/[pin]/+layout.server.js
+++ src/routes/[pin]/+layout.server.js
@@ -7,7 +7,14 @@ export async function load({ params }) {
     .ref(`auctions/${params.pin}/size`)
     .get()
   const size = auctionSize.val()
-  const colors = ["purple", "yellow", "brown", "gray", "lightblue", "orange"]
+  const colors = [
+    "#858D8D",
+    "#8338EC",
+    "#F77F00",
+    "#FF338B",
+    "#7C472E",
+    "#F2BB05",
+  ]
   colors.length = size
   return {
     size: size,
