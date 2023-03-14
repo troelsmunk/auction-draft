552830be50a1ca214cba02d177eade87b13933ff
Troels Munk <tm@troelsmunk.com>
2023-03-14 18:00:48 +0100

    Comparing bids with current winning bids

--- functions/findWinners.js
+++ functions/findWinners.js
@@ -11,18 +11,43 @@ module.exports = async function findWinners(readysChange, context) {
   const everyoneIsReady = await checkReadiness(roundRef, readysChange.after)
   if (!everyoneIsReady) return
 
+  let winnersAndBids = {
+    1: { uid: null, bid: -1 },
+    2: { uid: null, bid: -1 },
+    3: { uid: null, bid: -1 },
+    4: { uid: null, bid: -1 },
+    5: { uid: null, bid: -1 },
+    6: { uid: null, bid: -1 },
+    7: { uid: null, bid: -1 },
+    8: { uid: null, bid: -1 },
+    9: { uid: null, bid: -1 },
+    10: { uid: null, bid: -1 },
+    11: { uid: null, bid: -1 },
+    12: { uid: null, bid: -1 },
+    13: { uid: null, bid: -1 },
+    14: { uid: null, bid: -1 },
+    15: { uid: null, bid: -1 },
+  }
   const bids = await auctionRef
     .child("bids")
     .get()
     .then((snap) => snap.val())
-
-  for (const [uid, cards] of Object.entries(bids)) {
-    for (const [card, bid] of Object.entries(cards)) {
+  const orderedBidders = Object.keys(bids)
+  for (const uid of orderedBidders) {
+    for (const [card, bid] of Object.entries(bids[uid])) {
       console.log(`User bid ${bid} on card ${card} with uid ${uid}`)
+      if (bid > winnersAndBids[card].bid) {
+        console.log("New bid takes the lead!")
+        winnersAndBids[card] = {
+          uid: uid,
+          bid: bid,
+        }
+      }
     }
   }
 
   const resultRoundRef = auctionRef.child(`results/rounds/${currentRound}`)
+  return resultRoundRef.set(winnersAndBids)
 }
 
 /**
