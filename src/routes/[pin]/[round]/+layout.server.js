0014feaf9530da4337d3fc4454eaa1790049b47a
Troels Munk <tm@troelsmunk.com>
2023-05-05 18:04:53 +0200

    Validate values in pin/round/layout.server.js

--- src/routes/[pin]/[round]/+layout.server.js
+++ src/routes/[pin]/[round]/+layout.server.js
@@ -1,13 +1,21 @@
 import { admin } from "$lib/admin.server"
+import { error } from "@sveltejs/kit"
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ params }) {
-  const scoreboardSnap = await admin
+  if (!params || !params.round || !params.pin) {
+    throw error(500, "params didn't hold round and pin: " + params)
+  }
+  const scoreboard = await admin
     .database()
     .ref(`auctions/${params.pin}/scoreboard`)
     .get()
+    .then((snap) => snap.val())
+  if (!scoreboard || Object.keys(scoreboard).length < 1) {
+    throw error(500, "Invalid scoreboard from database: " + scoreboard)
+  }
   return {
-    scores: scoreboardSnap.val(),
+    scores: scoreboard,
     round: params.round,
   }
 }
