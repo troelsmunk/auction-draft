6b8c688bac7a40708a1250dec9b45368e329cf23
Troels Munk <tm@troelsmunk.com>
2023-05-05 19:39:35 +0200

    Validate data in pin/round/page.server.js

--- src/routes/[pin]/[round]/+page.server.js
+++ src/routes/[pin]/[round]/+page.server.js
@@ -9,6 +9,18 @@ export const actions = {
     const uid = event.cookies.get(COOKIE_NAME)
     const round = parseInt(event.params.round)
     const pin = parseInt(event.params.pin)
+    if (!bids || !uid || !round || !pin) {
+      console.error(
+        "Error: Invalid data. formData: %s, bids from formData: %s, uid from cookie: %s, " +
+          "round from params: %s, pin from params: %s",
+        formData,
+        bids,
+        uid,
+        round,
+        pin
+      )
+      return { success: false }
+    }
     await admin.database().ref(`auctions/${pin}/bids/${uid}`).set(bids)
     await admin.database().ref(`auctions/${pin}/readys/${uid}`).set(round)
     return { success: true }
