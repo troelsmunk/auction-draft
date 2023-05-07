854586e191432fa23e461f555ed4ee1ea248c2d7
Troels Munk <tm@troelsmunk.com>
2023-05-07 21:21:02 +0200

    Remove unused cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,12 @@
 const functions = require("firebase-functions")
-const createAuctionFunction = require("./createAuction")
-const createBidderFunction = require("./createBidder")
 const findWinnersFunction = require("./findWinners")
 const kitSsrFunction = require("./kitSSR")
 
 const functionsRegion = functions.region("europe-west1")
 
 const kitSSR = functionsRegion.https.onRequest(kitSsrFunction)
-const createAuction = functionsRegion.database
-  .ref("index/{uid}/auctionSize")
-  .onCreate(createAuctionFunction)
-const createBidder = functionsRegion.database
-  .ref("index/{uid}/pin")
-  .onCreate(createBidderFunction)
 const findWinners = functionsRegion.database
   .ref("auctions/{pin}/readys")
   .onUpdate(findWinnersFunction)
 
-module.exports = { kitSSR, createAuction, createBidder, findWinners }
+module.exports = { kitSSR, findWinners }
