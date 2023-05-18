e60ec1857afc72828f661dca3340e6156820aca7
Troels Munk <tm@troelsmunk.com>
2023-05-18 12:37:20 +0200

    Throw error on scoreboard load

--- src/routes/[pin]/[round]/+layout.server.js
+++ src/routes/[pin]/[round]/+layout.server.js
@@ -1,20 +1,17 @@
 import { admin } from "$lib/admin.server"
-import { logIfFalsy } from "$lib/validation"
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ params }) {
-  const pin = params?.pin
-  const round = params?.round
-  if (logIfFalsy(round, "round from params")) return
-  if (logIfFalsy(pin, "pin from params")) return
   const scoreboard = await admin
     .database()
-    .ref(`auctions/${pin}/scoreboard`)
+    .ref(`auctions/${params?.pin}/scoreboard`)
     .get()
     .then((snap) => snap.val())
-  logIfFalsy(scoreboard, "scoreboard from database")
+  if (!scoreboard) {
+    throw error(500, "The auction has no scoreboard")
+  }
   return {
     scores: scoreboard,
-    round: round,
+    round: params?.round,
   }
 }
