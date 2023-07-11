42f8ae3ccff36a91d49a50931bbf1344bbdcf1d5
Troels Munk <tm@troelsmunk.com>
2023-07-11 19:45:45 +0200

    Change to 5 columns in the grid

--- src/routes/[pin=int]/[round=int]/BidButtons.svelte
+++ src/routes/[pin=int]/[round=int]/BidButtons.svelte
@@ -17,7 +17,7 @@
     /* allow bid and buttons in the same space */
     display: grid;
     grid-template-rows: repeat(4, 1fr);
-    grid-template-columns: 1fr;
+    grid-template-columns: repeat(5, 1fr);
     grid-column-gap: 0px;
     grid-row-gap: 0px;
   }
@@ -30,18 +30,20 @@
   }
 
   button.plus-five {
-    grid-area: 1 / 1 / 2 / 2;
+    grid-area: 1 / 1 / 2 / 6;
   }
   button.plus-one {
-    grid-area: 2 / 1 / 4 / 2;
+    grid-area: 2 / 1 / 4 / 6;
   }
   button.minus-one {
-    grid-area: 4 / 1 / 5 / 2;
+    grid-area: 4 / 1 / 5 / 6;
   }
 
   .bid {
-    grid-area: 1 / 1 / 5 / 2;
+    grid-area: 1 / 2 / 5 / 6;
     font-size: xx-large;
+    text-align: center;
+    align-self: center;
     /* text a little to the right */
   }
 </style>
