0c91a53038b2915095922169820b6af4058ad96b
Troels Munk <tm@troelsmunk.com>
2023-07-11 18:39:01 +0200

    Attempt at styling buttons

--- src/routes/[pin=int]/[round=int]/BidButtons.svelte
+++ src/routes/[pin=int]/[round=int]/BidButtons.svelte
@@ -2,17 +2,40 @@
   export let bid = false
 </script>
 
-<div class="input-div">
-  <button on:click|preventDefault={() => (bid += 5)}>+5</button>
-  <button on:click|preventDefault={() => bid++}>+</button>
-  <button on:click|preventDefault={() => bid--}>-</button>
+<div class="outer">
+  <div class="buttons">
+    <button class="small" on:click|preventDefault={() => (bid += 5)}>+5</button>
+    <button on:click|preventDefault={() => bid++}>+</button>
+    <button class="small" on:click|preventDefault={() => bid--}>-</button>
+  </div>
+  <span class="bid">{bid}</span>
 </div>
-<span>{bid}</span>
 
 <style>
-  .input-div {
-    margin-top: 8px;
+  .outer {
+    margin: 5px;
+    /* allow bid and buttons in the same space */
+  }
+
+  .buttons {
     display: grid;
     grid-template-rows: 1fr 2fr 1fr;
+  }
+
+  button {
+    text-align: left;
+    border-left: none;
+    border-right: none;
+    /* faded out */
+  }
+
+  button.small {
+    border: none;
+  }
+
+  .bid {
+    grid-row: 1 / 4;
+    font-size: larger;
+    /* text a little to the right */
   }
 </style>
