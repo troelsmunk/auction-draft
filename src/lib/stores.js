f7979b2a6628b026d3edd702e8c4b0e0b9d8cc08
Troels Munk <tm@troelsmunk.com>
2023-07-16 20:34:02 +0200

    Remove unused stores

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,10 +1,8 @@
-import { derived, writable } from "svelte/store"
+import { writable } from "svelte/store"
 
 /** @type {import("svelte/store").Writable<string>} */
 export const uid = writable(null)
 /** @type {import("svelte/store").Writable<import("@firebase/app").FirebaseApp>} */
 export const firebaseApp = writable(null)
-export const auctionSize = writable(0)
-export const seat = writable(0)
 /** @type {import("svelte/store").Writable<number>} */
 export const currentRound = writable(0)
