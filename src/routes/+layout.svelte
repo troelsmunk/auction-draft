187735f17aba5a905c8bb5b82f356a61e0692853
Troels Munk <tm@troelsmunk.com>
2023-07-18 20:13:30 +0200

    Inline toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,15 +29,19 @@
 </script>
 
 {#if $pin}
-  <p>
-    <a href="/"> Home </a>
-    - Auction {$pin}
-    {#if $round}
-      - Round {$round}
-    {/if}
-  </p>
-  <label for="bid-method">Buttons!</label>
-  <input id="bid-method" type="checkbox" bind:checked={$bidByButtons} />
+  <div class="header-container">
+    <div class="breadcrumb">
+      <a href="/"> Home </a>
+      - Auction {$pin}
+      {#if $round}
+        - Round {$round}
+      {/if}
+    </div>
+    <div class="feature-toggle">
+      <label for="bid-method">Buttons!</label>
+      <input id="bid-method" type="checkbox" bind:checked={$bidByButtons} />
+    </div>
+  </div>
 {:else}
   <h1>Blind Auction Drafting</h1>
 {/if}
@@ -50,5 +54,13 @@
   ::global(*) {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
       Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  }
+
+  .header-container {
+    display: grid;
+    grid-template-columns: auto auto;
+  }
+  .feature-toggle {
+    justify-self: right;
   }
 </style>
