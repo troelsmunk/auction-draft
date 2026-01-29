16db378e7f44447fa4371de8ce7d6572270a6486
Troels Munk <tm@troelsmunk.com>
2026-01-29 19:09:59 +0100

    Install cloudflare. Remove Prettier temporarily.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { searchForWorkspaceRoot } from "vite"
-import { sveltekit } from "@sveltejs/kit/vite"
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-export default {
-  plugins: [sveltekit()],
-  server: {
-    fs: {
-      allow: [searchForWorkspaceRoot(process.cwd())],
-    },
-  },
-}
+export default defineConfig({
+	plugins: [sveltekit()]
+});
