31f3c0a475418cd2c5cb6600be317d05bf0bb215
Troels Munk <tm@troelsmunk.com>
2026-02-01 19:39:43 +0100

    Moved alias paths to please svelte build

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,10 @@ import adapter from "@sveltejs/adapter-cloudflare"
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    alias: {
+      $lib: "src/lib",
+      "$lib/*": "src/lib/*",
+    },
     adapter: adapter(),
   },
   extends: "./.svelte-kit/tsconfig.json",
