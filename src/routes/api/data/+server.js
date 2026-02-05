66b703c043ce193222b4f31017748726f168e477
Troels Munk <tm@troelsmunk.com>
2026-02-05 21:48:15 +0100

    Implement solution from https://github.com/cloudflare/workers-sdk/issues/9438#issuecomment-2930809905

--- src/routes/api/data/+server.js
+++ src/routes/api/data/+server.js
@@ -2,17 +2,39 @@ import { registerConnection } from "$lib/eventManager.js"
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const GET = ({ request, platform }) => {
+  /** @type {() => void} */
+  let unregister
+  /** @type {NodeJS.Timeout} */
+  let keepAlive
+  const { promise, resolve } = Promise.withResolvers()
+  platform?.ctx?.waitUntil(promise)
+
+  request.signal.addEventListener("abort", () => {
+    console.log("Request aborted!")
+    unregister?.()
+    clearInterval(keepAlive)
+    resolve("")
+  })
+
   const stream = new ReadableStream({
     start(controller) {
-      registerConnection(controller)
-      const unregister = registerConnection(controller)
+      console.log("SSE: Client connected")
+      unregister = registerConnection(controller)
       const encodedPing = new TextEncoder().encode(": ping\n")
-      const keepAlive = setInterval(() => {
+      keepAlive = setInterval(() => {
         try {
           controller.enqueue(encodedPing)
-        } catch {
+        } catch (e) {
+          console.error("SSE: Error enqueuing data:", e)
           clearInterval(keepAlive)
           unregister()
+          // Attempt to close if not already closed by 'cancel'
+          try {
+            console.log("SSE: attempting manual close")
+            controller.close()
+          } catch {
+            console.error("SSE: Error closing stream:", e)
+          }
         }
       }, 15000)
       controller.error = () => {
@@ -23,6 +45,16 @@ export const GET = ({ request, platform }) => {
         clearInterval(keepAlive)
         unregister()
       }
+    },
+    cancel(reason) {
+      // This is never called
+      console.log(
+        "SSE: Client disconnected (stream cancelled). Reason:",
+        reason,
+      )
+      clearInterval(keepAlive)
+      unregister?.()
+      resolve("")
     },
   })
   return new Response(stream, {
