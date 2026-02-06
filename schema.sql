077a4e1be368bd99fb42b5a9c615faa50d07d912
Troels Munk <tm@troelsmunk.com>
2026-02-06 16:54:34 +0100

    Corrected and updated db schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,12 @@
-DROP TABLE IF EXISTS auctions,
-users,
-bids,
-bid_options;
+DROP TABLE IF EXISTS Customers;
+
+DROP TABLE IF EXISTS auctions;
+
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS bids;
+
+DROP TABLE IF EXISTS bid_options;
 
 CREATE TABLE IF NOT EXISTS auctions (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,6 +20,8 @@ CREATE TABLE IF NOT EXISTS users (
   points_remaining INTEGER,
   bid_option_id INTEGER,
   seat_number INTEGER,
+  UNIQUE(auction_id, bid_option_id),
+  UNIQUE(auction_id, seat_number),
   FOREIGN KEY (auction_id) REFERENCES auctions(id),
   FOREIGN KEY (bid_option_id) REFERENCES bid_options(id)
 );
@@ -30,13 +37,15 @@ CREATE TABLE IF NOT EXISTS bids (
 
 CREATE TABLE IF NOT EXISTS bid_options (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  bid_options ARRAY of INTEGER,
+  bid_options_array ARRAY of INTEGER,
   size INTEGER
 );
 
 INSERT INTO
-  bid_options (bid_options, size)
+  bid_options (bid_options_array, size)
 VALUES
+  ('[10,22,49,110,246,548]', 2),
+  ('[14,33,74,164,367,818]', 2),
   ('[10,22,49,110,246,548]', 4),
   ('[12,27,60,135,300,670]', 4),
   ('[14,33,74,164,367,818]', 4),
