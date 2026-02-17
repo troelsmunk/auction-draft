452bbadd4a14ca1b366f0e560d39602e47eb25a9
Troels Munk <tm@troelsmunk.com>
2026-02-17 11:51:05 +0100

    Use bid options to convert bids upon submission

--- src/routes/[auction_number=int]/[round=int]/+page.server.js
+++ src/routes/[auction_number=int]/[round=int]/+page.server.js
@@ -1,4 +1,4 @@
-import { COOKIE_NAME } from "$lib/constants"
+import { BID_OPTIONS, COOKIE_NAME } from "$lib/constants"
 import { broadcastUpdate } from "$lib/sseManager"
 import { error, fail } from "@sveltejs/kit"
 
@@ -12,7 +12,6 @@ export const actions = {
     const formData = await event.request.formData()
     /** @type {Array<number|null>} bids */
     const bids = JSON.parse(formData?.get("bids")?.toString() || "[]")
-    const round = parseInt(event.params.round)
     const filteredBids = bids.map((bid) => {
       return bid || 0
     })
@@ -30,25 +29,53 @@ export const actions = {
       console.error("Error: Could not connect to database.")
       return error(500, "Database error")
     }
-    /** @type {number|null} */
-    const pointsRemaining = await db
-      .prepare("SELECT points_remaining FROM users WHERE uid = ?")
+    /** @type {Record<string, number|null>|null} */
+    const userSelect = await db
+      .prepare(
+        "SELECT points_remaining, seat_number, auction_id FROM users WHERE uid = ?",
+      )
       .bind(uid)
-      .first("points_remaining")
+      .first()
+    const pointsRemaining = userSelect?.points_remaining
     if (typeof pointsRemaining != "number") {
       console.error("Could not find points for UID: ", uid)
       return error(500, "Database error")
     }
-    const sumOfBids = filteredBids.reduce((sum, value) => sum + value)
+    const seat = userSelect?.seat_number
+    if (typeof seat != "number") {
+      return error(500, "Database error")
+    }
+    const auctionId = userSelect?.auction_id
+    /** @type {number|null} */
+    const auctionSize = await db
+      .prepare("SELECT count(1) as count FROM users WHERE auction_id = ?")
+      .bind(auctionId)
+      .first("count")
+    if (typeof auctionSize != "number") {
+      console.error(
+        "Could not find size for auction_id: ",
+        auctionId,
+        " related to UID: ",
+        uid,
+      )
+      return error(500, "Database error")
+    }
+    const optionsForThisUser = BID_OPTIONS.get(auctionSize)?.at(seat)
+    const bidsConvertedToOptions = filteredBids.map((bid) => {
+      return optionsForThisUser?.at(bid) || 0
+    })
+    const sumOfBids = bidsConvertedToOptions.reduce((sum, value) => sum + value)
     if (pointsRemaining < sumOfBids) {
       return fail(400, {
         error: "Insufficient funds",
-        bids: bids,
+        bids: bidsConvertedToOptions,
       })
     }
     const update = {
       uid: uid,
-      bids: sumOfBids,
+      bids: bidsConvertedToOptions,
+      sumOfBids: sumOfBids,
+      pointsRemaining: pointsRemaining,
     }
     broadcastUpdate(update, parseInt(event.params.auction_number))
   },
