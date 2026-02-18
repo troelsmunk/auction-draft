ee4e0bdaff157cdf6b8950d01baaf4ece4514f97
Troels Munk <tm@troelsmunk.com>
2026-02-18 12:21:29 +0100

    Remove the +5 button

--- src/routes/[auction_number=int]/[round=int]/BidButtons.svelte
+++ src/routes/[auction_number=int]/[round=int]/BidButtons.svelte
@@ -10,11 +10,6 @@
   let { bidValue = $bindable(), index, options } = $props()
 
   /** @type { (event: Event) => void  }*/
-  function plusFive(event) {
-    event.preventDefault()
-    bidValue += 5
-  }
-  /** @type { (event: Event) => void  }*/
   function plusOne(event) {
     event.preventDefault()
     bidValue++
@@ -34,16 +29,8 @@
       {options.at(bidValue)}
     {/if}
   </span>
-  <label for="plus-five-{index}" class="plus-five">+5</label>
   <label for="plus-one-{index}" class="plus-one">+</label>
   <label for="minus-one-{index}" class="minus-one">-</label>
-  <button
-    id="plus-five-{index}"
-    class="plus-five"
-    aria-labelledby="plus-five-item-{index}"
-    onclick={(event) => plusFive(event)}
-  >
-  </button>
   <button
     id="plus-one-{index}"
     class="plus-one"
@@ -65,7 +52,7 @@
     margin: 0.3em;
     border: 1px darkgray solid;
     display: grid;
-    grid-template-rows: repeat(4, 1.5em);
+    grid-template-rows: repeat(6, 1em);
     grid-template-columns: repeat(5, 1fr);
   }
 
@@ -78,30 +65,23 @@
     text-align: center;
   }
 
-  label.plus-five {
-    font-size: small;
-    grid-area: 1 / 1 / 2 / 3;
-  }
   label.plus-one {
     font-size: large;
-    grid-area: 2 / 1 / 4 / 3;
+    grid-area: 1 / 1 / 5 / 3;
     align-self: center;
   }
   label.minus-one {
     font-size: large;
-    grid-area: 4 / 1 / 5 / 3;
-  }
-  button.plus-five {
-    grid-area: 1 / 1 / 2 / 6;
+    grid-area: 5 / 1 / 7 / 3;
   }
   button.plus-one {
     border-width: 2px 0px;
     border-style: solid;
     border-image: linear-gradient(to right, darkgray, transparent) 6;
-    grid-area: 2 / 1 / 4 / 6;
+    grid-area: 1 / 1 / 5 / 6;
   }
   button.minus-one {
-    grid-area: 4 / 1 / 5 / 6;
+    grid-area: 5 / 1 / 7 / 6;
   }
 
   .bid {
