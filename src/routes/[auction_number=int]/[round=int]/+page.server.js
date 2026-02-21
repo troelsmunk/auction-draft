c3ed81a7bb5546243d1be9b1ca7fc0d10ad4f893
Troels Munk <tm@troelsmunk.com>
2026-02-21 12:19:25 +0100

    Finding winners introduced

--- src/routes/[auction_number=int]/[round=int]/+page.server.js
+++ src/routes/[auction_number=int]/[round=int]/+page.server.js
@@ -74,31 +74,75 @@ export const actions = {
         bids: bidsConvertedToOptions,
       })
     }
-    const round = event.params.round
-    const stringifiedBids = JSON.stringify(bidsConvertedToOptions)
-    const writeToDb = await db
+    const round = parseInt(event.params.round)
+    const insertBids = await db
       .prepare(
         "INSERT INTO bids (user_id, round, bid_values) VALUES (?,?,json(?)) " +
           "ON CONFLICT (user_id, round) DO UPDATE SET bid_values = excluded.bid_values",
       )
-      .bind(userId, round, stringifiedBids)
+      .bind(userId, round, JSON.stringify(bidsConvertedToOptions))
       .run()
-    if (writeToDb.error) {
+    if (insertBids.error) {
       console.error("Failed to write bids to database for uid: ", uid)
       return error(500, "Database error")
     }
     const auctionNumber = parseInt(event.params.auction_number)
-    const numberOfReadyBidders = await db
+    /**
+     * @typedef {Object} SeatAndBidsRow
+     * @property {number} seat
+     * @property {string} bids
+     */
+    const selectBids = await db
       .prepare(
-        "SELECT users.seat_number, bids.bid_values FROM users JOIN bids ON users.id = bids.user_id " +
+        "SELECT users.seat_number as seat, bids.bid_values as bids " +
+          "FROM users JOIN bids ON users.id = bids.user_id " +
           "WHERE users.auction_id = ? AND bids.round = ?",
       )
       .bind(auctionId, round)
       .run()
-    if (numberOfReadyBidders.results.length === auctionSize) {
-      const update = { newRound: 123 }
-      broadcastUpdate(update, auctionNumber)
+    if (selectBids.results.length === auctionSize) {
+      /**
+       * @typedef {Object} ResultForItem
+       * @property {number|null} seat
+       * @property {number} bid
+       */
+
+      const numberOfBids = bidsConvertedToOptions.length
+
+      /** @type {ResultForItem[]} */
+      let results = Array(numberOfBids)
+      console.log("empty, results: ", results)
+      results.fill({ seat: null, bid: 0 })
+      results.forEach((result, index) => {
+        results[index] = { seat: null, bid: 0 }
+      })
+      console.log("filled, results: ", results)
+      results[2].bid = 2
+      console.log("one set, results: ", results)
+
+      selectBids.results.forEach((/** @type {SeatAndBidsRow}*/ record) => {
+        const thisSeat = record.seat
+        const theseBids = JSON.parse(record.bids)
+        results.forEach((resultForItem, index) => {
+          if (resultForItem.bid < theseBids[index]) {
+            resultForItem.seat = thisSeat
+            resultForItem.bid = theseBids[index]
+          }
+        })
+      })
+      console.log("calculated, results: ", results)
+      const insertResults = await db
+        .prepare(
+          "INSERT INTO results (auction_id, round, results) VALUES (?,?,json(?)) " +
+            "ON CONFLICT (auction_id, round) DO NOTHING",
+        )
+        .bind(auctionId, round, JSON.stringify(results))
+        .run()
+      if (insertResults.success) {
+        const update = { newRound: round + 1 }
+        broadcastUpdate(update, auctionNumber)
+      }
     }
-    return { success: writeToDb.success }
+    return { success: insertBids.success }
   },
 }
